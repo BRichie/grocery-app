e42962263c27836a4916e1499d194230d439fd87
Brandon <brandoncrichie@gmail.com>
2019-04-04 13:40:16 -0700

    WIP deploy to production

---
+++ spec/integration/static_spec.js
@@ -0,0 +1,20 @@
+const request = require("request");
+const server = require("../../src/server");
+const base = "http://localhost:3000/";
+
+describe("routes : static", () => {
+
+  describe("GET /", () => {
+
+    it("should return status code 200 and have 'Systematic Shoppers' in the body", (done) => {
+
+      request.get(base, (err, res, body) => {
+        expect(res.statusCode).toBe(200);
+        expect(body).toContain("Systematic Shoppers");
+
+        done();
+      });
+    });
+
+  });
+});
